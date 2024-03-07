a6711e9ba07e961b9f7e8691a0c7fddbbd1815d2
spinerak <markvanderboor@hotmail.nl>
2024-03-07 19:50:02 +0100

    Update model.inc.c
    changed color to luigi

--- actors/mario/model.inc.c
+++ actors/mario/model.inc.c
@@ -2,13 +2,13 @@
 
 // 0x04000000 # solid color blue - butt, left thigh, right thigh - all poly types
 static const Lights1 mario_blue_lights_group = gdSPDefLights1(
-    0x00, 0x00, 0x7f,
+    0x2a, 0x4f, 0x8e, // RGB values
     0x00, 0x00, 0xff, 0x28, 0x28, 0x28
 );
 
 // 0x04000018 # solid color red - left & right arm, torso (tshirt part), caps - all poly types
 static const Lights1 mario_red_lights_group = gdSPDefLights1(
-    0x7f, 0x00, 0x00,
+    0x21, 0xac, 0x4a, // RGB values
     0xff, 0x00, 0x00, 0x28, 0x28, 0x28
 );
 
